a8a19420874836e6b16f7bb92c35456b3a02665a
Jared <kimbeljared@gmail.com>
2019-05-18 08:56:02 -0700

    More changes to mathInEngineering

--- mathInEngineering.py
+++ mathInEngineering.py
@@ -124,12 +124,14 @@ class problemAtHand(GraphScene):
         self.play(
             ShowCreation(splitLine1),
             ShowCreation(splitLine2),
+            LaggedStart(*map(ShrinkToCenter, connectingLine)),
         )
         self.add(point1, point2) #forces the points back on top of line
         self.wait(4)
 
         #Create transition title
-        solution_Text = TextMobject("Formulating a solution")
+        solution_Text = TextMobject("Formulating a solution:")
+        solution_Text.to_corner(UP + LEFT)
 
         self.play(
             LaggedStart(*map(ShrinkToCenter, splitLine2)),
@@ -138,7 +140,7 @@ class problemAtHand(GraphScene):
             LaggedStart(*map(ShrinkToCenter, point1_lab)),
             LaggedStart(*map(ShrinkToCenter, point2)),
             LaggedStart(*map(ShrinkToCenter, point2_lab)),
-            LaggedStart(*map(ShrinkToCenter, connectingLine)),
+
             Transform(problem_text, solution_Text),
         )
         self.wait()
@@ -156,12 +158,270 @@ class formulatingSolution(GraphScene):
 
     def construct(self):
 
-        solution_Text_Start = TextMobject("Formulating a solution")
         solution_Text = TextMobject("Formulating a solution:")
         solution_Text.to_corner(UP + LEFT)
-        self.add(solution_Text_Start)
+        self.add(solution_Text)
+
+        #Draw axes
+        self.setup_axes(animate=False)
+        self.wait(2)
+
+        #Create connecting line
+        x = self.coords_to_point(2,2)
+        y = self.coords_to_point(8,8)
+        connectingLine= Line(x, y)
+
+        #Create transform segments
+        x1 = self.coords_to_point(2,2)
+        y1 = self.coords_to_point(3.5,3.5)
+        connectingLineSegment1 = Line(x1,y1)
+
+        x2 = self.coords_to_point(3.5,3.5)
+        y2 = self.coords_to_point(5,5)
+        connectingLineSegment2 = Line(x2,y2)
+
+        x3 = self.coords_to_point(5,5)
+        y3 = self.coords_to_point(6.5,6.5)
+        connectingLineSegment3 = Line(x3,y3)
+
+        x4 = self.coords_to_point(6.5,6.5)
+        y4 = self.coords_to_point(8,8)
+        connectingLineSegment4 = Line(x4,y4)
+
+        #Draws connecting line and transform segments to screen
+        self.play(
+            ShowCreation(connectingLine),
+        )
+        self.wait(.5)
+
+        self.add(connectingLineSegment1)
+        self.add(connectingLineSegment2)
+        self.add(connectingLineSegment3)
+        self.add(connectingLineSegment4)
 
         self.play(
-            Transform(solution_Text_Start, solution_Text),
+            FadeOut(connectingLine),
+        )
+        self.wait(.5)
+
+        #Create all the Tiered line segments
+        x1 = self.coords_to_point(2,2)
+        y1 = self.coords_to_point(3.5,3.5)
+        lineSegment1 = Line(x1,y1)
+
+        x2 = self.coords_to_point(3.5,3.5+.5)
+        y2 = self.coords_to_point(5,5+.5)
+        lineSegment2 = Line(x2,y2)
+
+        x3 = self.coords_to_point(5,5+1)
+        y3 = self.coords_to_point(6.5,6.5+1)
+        lineSegment3 = Line(x3,y3)
+
+        x4 = self.coords_to_point(6.5,6.5+1.5)
+        y4 = self.coords_to_point(8,8+1.5)
+        lineSegment4 = Line(x4,y4)
+
+        self.play(
+            Transform(connectingLineSegment1, lineSegment1),
+            Transform(connectingLineSegment2, lineSegment2),
+            Transform(connectingLineSegment3, lineSegment3),
+            Transform(connectingLineSegment4, lineSegment4),
         )
         self.wait()
+
+class formulatingSolution2(GraphScene):
+    CONFIG = {
+        "x_min": 0,
+        "x_max": 10,
+        "y_min": 0,
+        "y_max": 10,
+        "graph_origin": ((DOWN*3.5)+(LEFT*4)),
+        "function_color": WHITE,
+        "axes_color": WHITE
+    }
+
+    def construct(self):
+
+        solution_Text = TextMobject("Formulating a solution:")
+        solution_Text.to_corner(UP + LEFT)
+        self.add(solution_Text)
+
+        #Draw axes
+        self.setup_axes(animate=False)
+
+        #Create line segments
+        x1 = self.coords_to_point(2,2)
+        y1 = self.coords_to_point(3.5,3.5)
+        lineSegment1 = Line(x1,y1)
+
+        x2 = self.coords_to_point(3.5,3.5+.5)
+        y2 = self.coords_to_point(5,5+.5)
+        lineSegment2 = Line(x2,y2)
+
+        x3 = self.coords_to_point(5,5+1)
+        y3 = self.coords_to_point(6.5,6.5+1)
+        lineSegment3 = Line(x3,y3)
+
+        x4 = self.coords_to_point(6.5,6.5+1.5)
+        y4 = self.coords_to_point(8,8+1.5)
+        lineSegment4 = Line(x4,y4)
+
+        self.add(lineSegment1)
+        self.add(lineSegment2)
+        self.add(lineSegment3)
+        self.add(lineSegment4)
+        self.wait(2)
+
+        #Create transform segments
+        x1 = self.coords_to_point(2,2)
+        y1 = self.coords_to_point(3.5,3.5)
+        connectingLineSegment1 = Line(x1,y1)
+
+        x2 = self.coords_to_point(3.5,3.5)
+        y2 = self.coords_to_point(5,5)
+        connectingLineSegment2 = Line(x2,y2)
+
+        x3 = self.coords_to_point(5,5)
+        y3 = self.coords_to_point(6.5,6.5)
+        connectingLineSegment3 = Line(x3,y3)
+
+        x4 = self.coords_to_point(6.5,6.5)
+        y4 = self.coords_to_point(8,8)
+        connectingLineSegment4 = Line(x4,y4)
+
+        self.play(
+            Transform(lineSegment1, connectingLineSegment1),
+            Transform(lineSegment2, connectingLineSegment2),
+            Transform(lineSegment3, connectingLineSegment3),
+            Transform(lineSegment4, connectingLineSegment4),
+        )
+        self.wait()
+
+class solvingProblem(GraphScene):
+    CONFIG = {
+        "x_min": 0,
+        "x_max": 10,
+        "y_min": 0,
+        "y_max": 10,
+        "graph_origin": ((DOWN*3.5)+(LEFT*4)),
+        "function_color": WHITE,
+        "axes_color": WHITE
+    }
+
+    def construct(self):
+
+        solution_Text = TextMobject("Formulating a solution:")
+        solution_Text.to_corner(UP + LEFT)
+        self.add(solution_Text)
+
+        #Draw axes
+        self.setup_axes(animate=False)
+
+        solving_text = TextMobject("Solving our problem:")
+        solving_text.to_corner(UP + LEFT)
+
+        #Create line
+        x = self.coords_to_point(2,2)
+        y = self.coords_to_point(8,8)
+        connectingLine= Line(x, y)
+
+        self.add(connectingLine)
+        self.wait(2)
+
+        self.play(
+            Transform(solution_Text, solving_text),
+        )
+        self.wait(1)
+
+        #Creates our start point and label
+        point1 = Dot(self.coords_to_point(2,2))
+        point1_lab = TextMobject("\\small{(Sx,Sy)}")
+        point1_lab.next_to(point1,RIGHT+DOWN)
+
+        #Creates our end point and label
+        point2 = Dot(self.coords_to_point(8,8))
+        point2_lab = TextMobject("\\small{(Ex,Ey)}")
+        point2_lab.next_to(point2,RIGHT+DOWN)
+
+        self.play(
+            ShowCreation(point1),
+            Write(point1_lab),
+            ShowCreation(point2),
+            Write(point2_lab),
+        )
+        self.wait(2)
+
+        #But wait theres a few more points
+        point3 = Dot(self.coords_to_point(5,2))
+        point3_lab = TextMobject("\\small{(Sx+$\\Delta$x,Sy)}")
+        point3_lab.next_to(point3,RIGHT+DOWN)
+
+        point4 = Dot(self.coords_to_point(8,2))
+        point4_lab = TextMobject("\\small{(Ex,Sy)}")
+        point4_lab.next_to(point4,RIGHT+DOWN)
+
+        point5 = Dot(self.coords_to_point(5,5))
+        point5_lab = TextMobject("\\small{(Sx+$\\Delta$x,Sy+$\\Delta$y)}")
+        point5_lab.next_to(point5,UP+LEFT)
+
+        self.play(
+            ShowCreation(point4),
+            Write(point4_lab),
+        )
+        self.wait()
+
+        #Horizontal line
+        horiz_x = self.coords_to_point(2,2)
+        horiz_y = self.coords_to_point(8,2)
+        horizontalConnectingLine= Line(horiz_x, horiz_y)
+
+        #Vertical line
+        vert_x = self.coords_to_point(8,2)
+        vert_y = self.coords_to_point(8,8)
+        verticalConnectingLine= Line(vert_x, vert_y)
+
+        #Center line
+        cent_x = self.coords_to_point(5,2)
+        cent_y = self.coords_to_point(5,5)
+        centerLine= Line(cent_x, cent_y)
+
+        self.play(
+            ShowCreation(horizontalConnectingLine),
+        )
+        self.wait(.5)
+
+        self.play(
+            ShowCreation(verticalConnectingLine),
+        )
+        self.wait(.5)
+
+        self.play(
+            ShowCreation(point3),
+            Write(point3_lab),
+        )
+        self.wait()
+
+        self.play(
+            ShowCreation(point5),
+            Write(point5_lab),
+        )
+        self.wait()
+
+        self.play(
+            ShowCreation(centerLine),
+        )
+        self.wait(2)
+
+        #Color stuff
+        self.play(
+            FadeToColor(point1, RED),
+            FadeToColor(point1_lab, RED),
+            FadeToColor(point2, BLUE),
+            FadeToColor(point2_lab, BLUE),
+            FadeToColor(point3, GREEN),
+            FadeToColor(point3_lab, GREEN),
+            FadeToColor(point4, ORANGE),
+            FadeToColor(point4_lab, ORANGE),
+            FadeToColor(point5, YELLOW),
+            FadeToColor(point5_lab, YELLOW),
+        )
